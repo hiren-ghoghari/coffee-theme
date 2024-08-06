406d505241ac5d352df4dd9702a5e8de2b5b4204
Nitin <nitinghoghari233@gmail.com>
2024-08-07 00:18:25 +0530

    Dynanic products integration

--- components/home/product-section.tsx
+++ components/home/product-section.tsx
@@ -1,7 +1,7 @@
 'use client'
 import ProductCard from '@/components/home/product-card'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import React from 'react'
+import React, { useState } from 'react'
 
 const products = [
   {
@@ -44,25 +44,113 @@ const products = [
     reviews: 390,
     rating: 4.9,
   },
+  {
+    name: 'Chocolate Robusta',
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Scelerisque urna vel sit dolor fringilla',
+    image: '/assets/images/product-1.png',
+    category: 'robusta',
+    price: 2.9,
+    reviews: 360,
+    rating: 4.9,
+  },
+  {
+    name: 'Mocha Robusta',
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Scelerisque urna vel sit dolor fringilla',
+    image: '/assets/images/product-2.png',
+    category: 'robusta',
+    price: 2.7,
+    reviews: 180,
+    rating: 5,
+  },
+  {
+    name: 'Coffee Robusta',
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Scelerisque urna vel sit dolor fringilla',
+    image: '/assets/images/product-3.png',
+    category: 'robusta',
+    price: 2.9,
+    reviews: 450,
+    rating: 4.8,
+  },
+  {
+    name: 'Hazelnut Robusta',
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Scelerisque urna vel sit dolor fringilla',
+    image: '/assets/images/product-4.png',
+    category: 'robusta',
+    price: 2.8,
+    reviews: 390,
+    rating: 4.9,
+  },
+  {
+    name: 'Chocolate Acabica',
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Scelerisque urna vel sit dolor fringilla',
+    image: '/assets/images/product-1.png',
+    category: 'acabica',
+    price: 2.9,
+    reviews: 360,
+    rating: 4.9,
+  },
+  {
+    name: 'Mocha Acabica',
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Scelerisque urna vel sit dolor fringilla',
+    image: '/assets/images/product-2.png',
+    category: 'acabica',
+    price: 2.7,
+    reviews: 180,
+    rating: 5,
+  },
+  {
+    name: 'Coffee Acabica',
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Scelerisque urna vel sit dolor fringilla',
+    image: '/assets/images/product-3.png',
+    category: 'acabica',
+    price: 2.9,
+    reviews: 450,
+    rating: 4.8,
+  },
+  {
+    name: 'Hazelnut Acabica',
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Scelerisque urna vel sit dolor fringilla',
+    image: '/assets/images/product-4.png',
+    category: 'acabica',
+    price: 2.8,
+    reviews: 390,
+    rating: 4.9,
+  },
 ]
 
 const ProductSection = () => {
+  const [activeTab, setActiveTab] = useState('latte')
   return (
     <div>
-      <Tabs defaultValue="latte" className="flex flex-col items-center gap-10">
+      <Tabs
+        defaultValue="latte"
+        value={activeTab}
+        onValueChange={setActiveTab}
+        className="flex flex-col items-center gap-10"
+      >
         <div>
-          <TabsList>
+          <TabsList className="flex-wrap">
             <TabsTrigger value="latte">Latte</TabsTrigger>
             <TabsTrigger value="robusta">Robusta</TabsTrigger>
             <TabsTrigger value="acabica">Acabica</TabsTrigger>
           </TabsList>
         </div>
 
-        <TabsContent value="latte">
-          <div className="flex gap-6">
-            {products.map((product, key) => {
-              return <ProductCard key={key} product={product} />
-            })}
+        <TabsContent value={activeTab}>
+          <div className="flex flex-wrap gap-6">
+            {products
+              .filter((product) => product.category === activeTab)
+              .map((product, key) => {
+                return <ProductCard key={key} product={product} />
+              })}
           </div>
         </TabsContent>
       </Tabs>
